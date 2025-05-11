7a4da2100d7eaa9a2c9ab406eb12c72488d0efd7
Vlad Korochanskiy <mginster234942@gmail.com>
2025-05-11 22:06:52 +0300

    asdas

--- shared/components/menu-mobile-bottom.tsx
+++ shared/components/menu-mobile-bottom.tsx
@@ -18,7 +18,7 @@ export const MenuMobileBottom: React.FC<Props> = ({
   cart,
 }) => {
   return (
-    <div className="sm:hidden fixed bottom-55 left-0 right-0 flex justify-around items-center bg-white border-t border-gray-200 z-50 py-2">
+    <div className="sm:hidden fixed bottom-0 left-0 right-0 flex justify-around items-center bg-white border-t border-gray-200 z-50 py-2">
       <Link href="/" className=" flex flex-col items-center gap-1">
         <House className="" />
         <span className="text-xs ">Главная</span>
