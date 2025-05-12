952327053962afc86dd176b9262ee08d013d3266
Vlad Korochanskiy <mginster234942@gmail.com>
2025-05-13 00:01:27 +0300

    asdasd

--- shared/components/menu-mobile-bottom.tsx
+++ shared/components/menu-mobile-bottom.tsx
@@ -25,7 +25,7 @@ export const MenuMobileBottom: React.FC<Props> = ({
   const fullname = useUserStore((state) => state.fullname);
 
   return (
-    <div className="sm:hidden fixed bottom-50 left-0 right-0 flex justify-around items-center bg-white border-t border-gray-200 z-50 py-2">
+    <div className="sm:hidden fixed bottom-0 left-0 right-0 flex justify-around items-center bg-white border-t border-gray-200 z-50 py-2">
       <Link href="/" className=" flex flex-col items-center gap-1">
         <House className="" />
         <span className="text-xs ">Главная</span>
