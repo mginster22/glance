32d61c40c360e2433d88599e9a852e4443414862
Vlad Korochanskiy <mginster234942@gmail.com>
2025-05-12 14:13:48 +0300

    asds

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Home() {
  
   return (
-    <div className="mt-4 max-sm:pb-4">
+    <div className="mt-4 max-sm:pb-20">
       <Banner />
       <Catalog />
       <Stock />
